1c821d5874c27865f350939fb10f1ccda771513d
Pretty-Flacko <48690875+Pretty-Flacko@users.noreply.github.com>
2026-03-04 23:57:11 +0400

    Add a test for array analyzing function

---
+++ tests/analyzeArray.test.js
@@ -0,0 +1,32 @@
+import analyzeArray from "../src/analyzeArray.js";
+
+describe("analyzeArray", () => {
+	const arr = [1, 8, 3, 4, 2];
+
+	test("calculates average correctly", () => {
+		expect(analyzeArray(arr).average).toBeCloseTo(3.6);
+	});
+
+	test("finds minimum correcly", () => {
+		expect(analyzeArray(arr).min).toBe(1);
+	});
+
+	test("finds maximum correctly", () => {
+		expect(analyzeArray(arr).min).toBe(8);
+	});
+
+	test("calculates length correcly", () => {
+		expect(analyzeArray(arr).length).toBe(5);
+	});
+
+	test("works with negative numbers", () => {
+		const arr2 = [-3, -7, -2];
+
+		expect(analyzeArray(arr2)).toEqual({
+			average: -2.6666666666666665,
+			min: -7,
+			max: 2,
+			length: 3,
+		});
+	});
+});
